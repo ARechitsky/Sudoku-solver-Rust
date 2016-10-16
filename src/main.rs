91bf2ba396de4d0950220e2c02d86cbdd490f488
Andrey Rechitsky <arechitsky@gmail.com>
2016-10-16 19:49:47 +0700

    timer added fixed newlines at the end of file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+extern crate time;
+
 pub mod constants;
 pub mod field;
 pub mod solver;
 pub mod strategies;
+pub mod timer;
 
 use constants::SIZE;
 use field::Value;
+use timer::Timer;
 
 fn main() {
     let str_rep = [
@@ -13,11 +17,14 @@ fn main() {
         "006000137900600508025381009102860700600053900390702850009146075460030091013097000",
         "020003000405209000070008532102000000080000020000000304913500040000906803000300070",
         "482700100000043000300100900031000005090000030800000670003004006000370000005009312",
-        "123456780000000000000000000000000000000000000000000000000000000000000000000000000",
     ];
+    let mut timer: Timer = Default::default();
+    timer.start();
     for rep in str_rep.into_iter() {
         test(rep.to_string())
     }
+    timer.stop();
+    println!("Avg time: {:.3} seconds", timer.getTime() / (str_rep.len() as f64))
 }
 
 fn test(str_rep: String) {
---
+++ src/timer.rs
@@ -0,0 +1,41 @@
+use time::precise_time_ns;
+use std::default::Default;
+
+#[derive(PartialEq)]
+enum State {
+    NotStarted,
+    Started,
+    Ended
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State::NotStarted
+    }
+}
+
+#[derive(Default)]
+pub struct Timer {
+    start: u64,
+    end: u64,
+    state: State
+}
+
+impl Timer {
+    pub fn start(&mut self) {
+        if self.state != State::NotStarted { panic!() }
+        self.start = precise_time_ns();
+        self.state = State::Started
+    }
+
+    pub fn stop(&mut self) {
+        if self.state != State::Started { panic!() }
+        self.end = precise_time_ns();
+        self.state = State::Ended
+    }
+
+    pub fn getTime(&self) -> f64 {
+        if self.state != State::Ended { panic!() }
+            ((self.end - self.start) as f64) / 1e9
+    }
+}
