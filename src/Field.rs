73d24070e56bee92e4da05ed14fadbeec5824b52
Andrey Rechitsky <arechitsky@gmail.com>
2016-10-17 20:33:34 +0700

    [optimization] replace cycle by cells for cycle by cell group

--- src/Field.rs
+++ src/Field.rs
@@ -57,25 +57,25 @@ impl Field {
     }
 
     /// Returns cell with index ```index``` in the same square, that contains ```(x, y)``` cell
-    fn get_cell_in_square(x: usize, y: usize, index: usize) -> (usize, usize) {
-        let square_x = x / BLOCK_H_SIZE;
-        let square_y = y / BLOCK_V_SIZE;
+    fn get_cell_in_square(square_index: usize, index: usize) -> (usize, usize) {
+        let square_x = square_index % BLOCK_H_COUNT;
+        let square_y = square_index / BLOCK_H_COUNT;
         let x_in_square = index % BLOCK_H_SIZE;
         let y_in_square = index / BLOCK_H_SIZE;
         (square_x * BLOCK_H_SIZE + x_in_square, square_y * BLOCK_V_SIZE + y_in_square)
     }
 
-    /// Returns cell with index ```index``` in the group of type ```group_type```,
-    /// that contains ```(x, y, z)``` cell
-    pub fn get_cell_in_group(x: usize, y: usize, z: usize,
+    /// Returns cell with index ```index```
+    /// in the group with index ```group_index``` of type ```group_type```
+    pub fn get_cell_in_group(group_index: usize,
                              group_type: &CellGroupType, index: usize) -> (usize, usize, usize) {
         match group_type {
-            &CellGroupType::XY => (x, y, index),
-            &CellGroupType::XZ => (x, index, z),
-            &CellGroupType::YZ => (index, y, z),
+            &CellGroupType::XY => (group_index / SIZE, group_index % SIZE, index),
+            &CellGroupType::XZ => (group_index / SIZE, index, group_index % SIZE),
+            &CellGroupType::YZ => (index, group_index / SIZE, group_index % SIZE),
             &CellGroupType::SquareZ => {
-                let (new_x, new_y) = Field::get_cell_in_square(x, y, index);
-                (new_x, new_y, z)
+                let (new_x, new_y) = Field::get_cell_in_square(group_index / SIZE, index);
+                (new_x, new_y, group_index % SIZE)
             }
         }
     }
--- src/strategies.rs
+++ src/strategies.rs
@@ -34,16 +34,15 @@ pub struct StrategyCrossOutSimple;
 impl BaseStrategy for StrategyCrossOutSimple {
     fn find_turns(&self, field: &Field) -> Vec<Vec<Move>> {
         let mut result = Vec::new();
-        for x in 0..SIZE {
-            for y in 0..SIZE {
-                for z in 0..SIZE {
+        for group in CellGroupType::iterator() {
+            for group_index in 0..SIZE * SIZE {
+                for cell in 0..SIZE {
+                    let (x, y, z) = Field::get_cell_in_group(group_index, group, cell);
                     if field.data[x][y][z] == Value::Yes {
-                        for group in CellGroupType::iterator() {
-                            for other in 0..SIZE {
-                                let (new_x, new_y, new_z) = Field::get_cell_in_group(x, y, z, group, other);
-                                if field.data[new_x][new_y][new_z] == Value::None {
-                                    result.push(BaseStrategy::definite_turn(new_x, new_y, new_z, Value::No))
-                                }
+                        for other in 0..SIZE {
+                            let (new_x, new_y, new_z) = Field::get_cell_in_group(group_index, group, other);
+                            if field.data[new_x][new_y][new_z] == Value::None {
+                                result.push(BaseStrategy::definite_turn(new_x, new_y, new_z, Value::No))
                             }
                         }
                     }
@@ -59,26 +58,22 @@ pub struct StrategyCheckSimple;
 impl BaseStrategy for StrategyCheckSimple {
     fn find_turns(&self, field: &Field) -> Vec<Vec<Move>> {
         let mut result = Vec::new();
-        for x in 0..SIZE {
-            for y in 0..SIZE {
-                for z in 0..SIZE {
-                    for group in CellGroupType::iterator() {
-                        let mut moves: Vec<Move> = Vec::new();
-                        let mut yes_count = 0;
-                        for other in 0..SIZE {
-                            let (new_x, new_y, new_z) = Field::get_cell_in_group(x, y, z, group, other);
-                            match field.data[new_x][new_y][new_z] {
-                                Value::None =>
-                                    moves.push(Move { x: new_x, y: new_y, z: new_z, value: Value::Yes }),
-                                Value::Yes =>
-                                    yes_count += 1,
-                                _ => {}
-                            }
-                        }
-                        if yes_count == 0 && moves.len() > 0 {
-                            result.push(moves)
-                        }
+        for group in CellGroupType::iterator() {
+            for group_index in 0..SIZE * SIZE {
+                let mut moves: Vec<Move> = Vec::new();
+                let mut yes_count = 0;
+                for other in 0..SIZE {
+                    let (new_x, new_y, new_z) = Field::get_cell_in_group(group_index, group, other);
+                    match field.data[new_x][new_y][new_z] {
+                        Value::None =>
+                            moves.push(Move { x: new_x, y: new_y, z: new_z, value: Value::Yes }),
+                        Value::Yes =>
+                            yes_count += 1,
+                        _ => {}
                     }
+                }
+                if yes_count == 0 && moves.len() > 0 {
+                    result.push(moves)
                 }
             }
         }
